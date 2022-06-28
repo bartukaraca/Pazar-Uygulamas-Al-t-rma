2f6f713bfdd99a565959ecc6551a1c784474c2b4
brt_k <brt_krc5353@hotmail.com>
2022-06-28 16:33:19 +0300

    interface

---
+++ OOP2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.MusteriNo = "123123";
+            musteri1.Adi = "Bartu";
+            musteri1.Soyadi = " Karaca";
+            musteri1.TcNo = "1231231231";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "567567";
+            musteri2.SirketAdi = "Karacaholding";
+            musteri2.VergiNo = "1241241242124";
+
+            Musteri musteri = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            İhtiyacKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+            TaşıtKrediManager taşıtKrediManager = new TaşıtKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new FileLoggerService(), new DataBaseLoggerService() };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, new List<ILoggerService> {new DataBaseLoggerService(), new FileLoggerService() } );
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager };
+            // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
